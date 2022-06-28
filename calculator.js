cfe8726dcb39a584d1325a1595a944a4078c2862
Martin <martindeliragalvan@outlook.com>
2022-06-27 21:22:00 -0500

    updated

--- calculator.js
+++ calculator.js
@@ -5,6 +5,20 @@ angular.module('MyCalculator',[])
 .controller('MyCalculatorController',function($scope){
 $scope.name = "";
 $scope.totalValue = 0;
+
+$scope.displayNumeric = function (){
+  var totalValueName = calculateNumericForString($scope.name);
+  $scope.totalValue = totalValueName;
+};
+
+function calculateNumericForString(string){
+  var totalStringValue = 0;
+  for (var i = 0; i < string.length; i++){
+    totalStringValue += string.charCodeAt(i);
+  }
+  return totalStringValue;
+}
+
 });
 
 })();
