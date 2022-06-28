6f32cff128e927a49fc94acf268fce7b4bcac93b
Martin <martindeliragalvan@outlook.com>
2022-06-28 00:38:23 -0500

    added extra feature

--- Assignment1/eatforlunch.js
+++ Assignment1/eatforlunch.js
@@ -3,28 +3,16 @@
 
 angular.module('MyLunch',[])
 .controller('MyLunchController',function($scope){
-$scope.food = "";
+  $scope.food = "";
+  $scope.totalFood = "";
 
-// initialized as empty, will take final value when
-// clicking button
-$scope.totalFood = "";
+    $scope.checkIfTooMuch = function (){
+      var foodList =  $scope.food.split(",").filter(function (el){ return el !=""})
 
-$scope.checkIfTooMuch = function (){
-  var foodList = $scope.food.split(",");
+      if($scope.food == ""){$scope.totalFood = "Please enter data first";}
+      else if (foodList.length>0 && foodList.length<=3) {$scope.totalFood = "Enjoy!";}
+      else if(foodList.length>3){$scope.totalFood = "Too Much!";}
 
-// if input box is empty, don't show any message
-  if($scope.food == ""){
-    $scope.totalFood = "";
-  }
-  else if (foodList.length>0 && foodList.length<=3) {
-    $scope.totalFood = "Enjoy!";
-  }
-  else if(foodList.length>3){
-  $scope.totalFood = "Too Much!";
-  }
-
-};
-
-});
-
+    };
+  });
 })();
