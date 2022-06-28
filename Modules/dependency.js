e4b4b4ca11772f6cc6805fd54d6b277d6469b32f
Martin <martindeliragalvan@outlook.com>
2022-06-27 23:07:09 -0500

    moved learning files to another dir

--- Modules/dependency.js
+++ Modules/dependency.js
@@ -4,13 +4,20 @@
 angular.module('DIApp',[])
 .controller('DIController',DIController);
 
-function DIController($scope, $filter) {
+function DIController($scope, $filter,$injector) {
   $scope.name="Martin";
 
   $scope.upper = function (){
     var upCase = $filter('uppercase');
     $scope.name = upCase($scope.name);
   };
+
+function AnnotateMe(name,job,blah) {
+  return "totoi";
+}
+console.log($injector.annotate(DIController));
+// console.log(DIController.toString());
+
 
 }
 
